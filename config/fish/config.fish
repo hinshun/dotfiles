27f6b381819af0f8c44905e8f0b8c0da0f358b82
Edgar Lee <edgarl@netflix.com>
2022-02-03 04:31:13 +0000

    Bump

---
+++ config/fish/config.fish
@@ -0,0 +1,9 @@
+# Startup ssh-agent
+if test -z (pgrep ssh-agent | string collect)
+    eval (ssh-agent -c)
+    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+end
+
+# Setup prompt
+starship init fish | source
